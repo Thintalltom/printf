fd70e655e7c796f21933f8bd1d03eabbbc9659ad
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-08-07 23:47:14 +0100

    Update main.h

--- main.h
+++ main.h
@@ -41,5 +41,11 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 #endif
