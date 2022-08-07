75b62c9d4cadefc072cc905102b0b96124692064
Thintalltom <105128485+Thintalltom@users.noreply.github.com>
2022-08-08 00:22:27 +0100

    Update main.h

--- main.h
+++ main.h
@@ -95,4 +95,13 @@ int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
 #endif
